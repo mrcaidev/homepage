4ce8b4768f3e647a4d7982557fa98cf88469acea
mrcaidev <mrcaidev@qq.com>
2022-07-24 10:54:22 +0800

    fix(component): i18n for nav links

--- src/components/top-bar/navigation-links.tsx
+++ src/components/top-bar/navigation-links.tsx
@@ -1,16 +1,21 @@
+import { useLocale } from "src/hooks/locale.hook";
 import { Link } from "../common/link";
 import navLinks from "./nav-links.json";
 
-export const NavigationLinks = () => (
-  <>
-    {navLinks.map(({ name, href }) => (
-      <Link
-        key={href}
-        href={href}
-        className="px-4 py-2 rounded-md text-lg font-semibold text-center transition-bg hover:bg-slate-200 hover:dark:bg-slate-800 active:bg-slate-300 active:dark:bg-slate-700"
-      >
-        {name}
-      </Link>
-    ))}
-  </>
-);
+export const NavigationLinks = () => {
+  const { locale } = useLocale();
+
+  return (
+    <>
+      {navLinks.map(({ en, zh, href }) => (
+        <Link
+          key={href}
+          href={href}
+          className="px-4 py-2 rounded-md text-lg font-semibold text-center transition-bg hover:bg-slate-200 hover:dark:bg-slate-800 active:bg-slate-300 active:dark:bg-slate-700"
+        >
+          {locale === "en-US" ? en : zh}
+        </Link>
+      ))}
+    </>
+  );
+};
