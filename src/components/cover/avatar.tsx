6b7e0aafa256b9f1b83bc844d8d5d04ff55c0f19
mrcaidev <mrcaidev@qq.com>
2022-07-27 20:41:27 +0800

    perf: remove lazy loading on first page

--- src/components/cover/avatar.tsx
+++ src/components/cover/avatar.tsx
@@ -9,6 +9,12 @@ export const Avatar = () => (
     transition={{ type: "tween", duration: 1 }}
     className="w-52 h-52 sm:w-60 sm:h-60 md:w-72 md:h-72"
   >
-    <Image src={miku} alt="Miku" placeholder="blur" className="rounded-3xl" />
+    <Image
+      src={miku}
+      alt="Miku"
+      priority
+      placeholder="blur"
+      className="rounded-3xl"
+    />
   </m.div>
 );
