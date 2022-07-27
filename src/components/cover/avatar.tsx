2f614e905e1ef1552fd17c238e8698c59f8c61b5
mrcaidev <mrcaidev@qq.com>
2022-07-27 21:05:32 +0800

    perf: remove lcp animation

--- src/components/cover/avatar.tsx
+++ src/components/cover/avatar.tsx
@@ -1,14 +1,8 @@
-import { m } from "framer-motion";
 import Image from "next/future/image";
 import miku from "public/miku.webp";
 
 export const Avatar = () => (
-  <m.div
-    initial={{ y: 30, opacity: 0 }}
-    whileInView={{ y: 0, opacity: 1 }}
-    transition={{ type: "tween", duration: 1 }}
-    className="w-52 h-52 sm:w-60 sm:h-60 md:w-72 md:h-72"
-  >
+  <div className="w-52 h-52 sm:w-60 sm:h-60 md:w-72 md:h-72">
     <Image
       src={miku}
       alt="Miku"
@@ -16,5 +10,5 @@ export const Avatar = () => (
       placeholder="blur"
       className="rounded-3xl"
     />
-  </m.div>
+  </div>
 );
