d7b06837d31f58838e0f4d18cec179d75da1b8b0
mrcaidev <mrcaidev@gmail.com>
2022-09-17 14:23:08 +0800

    build(dep): add astro integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
+import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://mrcai.space",
-  integrations: [react(), tailwind()],
+  site: "https://www.mrcai.space",
+  integrations: [react(), prefetch(), sitemap(), tailwind()],
 });
