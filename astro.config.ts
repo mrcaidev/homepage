1d557c16f33b4d5bc4284bf6fefdecbd2b87844d
mrcaidev <mrcaidev@gmail.com>
2022-09-17 14:23:08 +0800

    build(dep): add astro integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://mrcai.space",
-  integrations: [react(), tailwind()],
+  site: "https://www.mrcai.space",
+  integrations: [react(), image(), prefetch(), sitemap(), tailwind()],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["src/{components,pages}/**/*.{astro,tsx}"],
+  content: ["src/{components,layouts,pages}/**/*.{astro,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
