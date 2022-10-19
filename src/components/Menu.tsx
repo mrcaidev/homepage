64d4a4feb17f4bafc57bbbdf9326c2549625ea02
mrcaidev <mrcaidev@gmail.com>
2022-10-19 15:01:48 +0800

    refactor: better layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -23,7 +23,7 @@ const Menu = ({ children }: Props) => {
       </button>
       {isOpen && (
         <div class="relative -z-10">
-          <div class="flex flex-col justify-center items-center gap-12 fixed top-0 left-0 w-screen h-screen px-8 py-20 bg-normal border-b border-b-slate-700 animate-[menu_1s_ease]">
+          <div class="flex flex-col justify-center items-center gap-12 fixed top-0 left-0 w-screen h-screen px-8 py-20 bg-normal border-b border-b-slate-700 animate-[menu_0.6s_ease]">
             <nav class="flex flex-col gap-4">{children}</nav>
             <div class="flex gap-4">
               <ThemeToggler />
