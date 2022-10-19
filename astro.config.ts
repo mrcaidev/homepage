3054165a6222a516bc934f591374272110035f5f
mrcaidev <mrcaidev@gmail.com>
2022-10-19 11:06:37 +0800

    build: remove sitemap integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import preact from "@astrojs/preact";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mrcai.dev",
-  integrations: [preact(), tailwind(), sitemap()],
+  integrations: [preact(), tailwind()],
 });
