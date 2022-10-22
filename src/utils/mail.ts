8dac631d018e46d5632119d01cb110b16bb55144
mrcaidev <mrcaidev@gmail.com>
2022-10-22 18:34:22 +0800

    feat: contact page

---
+++ src/utils/mail.ts
@@ -0,0 +1,37 @@
+const apiUrl = "https://api.emailjs.com/api/v1.0/email/send";
+const serviceId = import.meta.env.PUBLIC_EMAILJS_SERVICE_ID || "";
+const templateId = import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID || "";
+const userId = import.meta.env.PUBLIC_EMAILJS_USER_ID || "";
+
+export interface Mail {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export async function sendMail(mail: Mail) {
+  const body = JSON.stringify({
+    service_id: serviceId,
+    template_id: templateId,
+    user_id: userId,
+    template_params: mail,
+  });
+
+  if (import.meta.env.DEV) {
+    return new Promise<boolean>((resolve) => {
+      const success = Math.random() < 0.5;
+      setTimeout(() => resolve(success), 1000);
+    });
+  }
+
+  try {
+    const res = await fetch(apiUrl, {
+      method: "POST",
+      body,
+      headers: { "Content-Type": "application/json" },
+    });
+    return res.status === 200;
+  } catch {
+    return false;
+  }
+}
