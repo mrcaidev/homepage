96b5b7afc9c4637199ab6d731bab154fd3a722cb
mrcaidev <mrcaidev@gmail.com>
2022-10-23 16:11:42 +0800

    refactor: styles

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -11,8 +11,8 @@ const ContactForm = () => {
     setIsSending(true);
 
     const formData = new FormData(e.target as HTMLFormElement);
-    const data = Object.fromEntries(formData) as unknown as Mail;
-    const success = await sendMail(data);
+    const mail = Object.fromEntries(formData) as unknown as Mail;
+    const success = await sendMail(mail);
 
     setIsSending(false);
     setIsSuccess(success);
@@ -25,23 +25,23 @@ const ContactForm = () => {
         name="name"
         placeholder="Your name"
         required
-        disabled={isSending}
-        class="block w-full px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:ring-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
+        disabled={isSending || isSuccess === true}
+        class="w-full px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:outline-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
       />
       <input
         type="email"
         name="email"
         placeholder="Your e-mail"
         required
-        disabled={isSending}
-        class="block w-full px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:ring-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
+        disabled={isSending || isSuccess === true}
+        class="w-full px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:outline-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
       />
       <textarea
         name="message"
         placeholder="Message"
         required
-        disabled={isSending}
-        class="block w-full min-h-[160px] px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:ring-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
+        disabled={isSending || isSuccess === true}
+        class="w-full min-h-[160px] px-0.5 py-2 border-0 border-b-2 border-slate-500 disabled:border-slate-300 dark:disabled:border-slate-700 bg-transparent focus:outline-0 focus:border-sky-800 dark:focus:border-sky-200 placeholder:text-slate-500 disabled:text-slate-500"
       />
       {isSuccess === true ? (
         <p class="w-full px-4 py-2 rounded-lg border-2 border-green-800 dark:border-green-200 font-medium text-green-800 dark:text-green-200 text-center">
@@ -51,12 +51,8 @@ const ContactForm = () => {
         <button
           type="submit"
           disabled={isSending}
-          class={`w-full px-4 py-2 rounded-lg bg-sky-800 dark:bg-sky-200 font-medium text-slate-200 dark:text-slate-800 shadow hover:brightness-90 transition-[filter]${
-            isSending
-              ? " bg-slate-800 dark:bg-slate-200"
-              : isSuccess === undefined
-              ? ""
-              : " bg-red-800 dark:bg-red-200"
+          class={`w-full px-4 py-2 rounded-lg bg-sky-800 dark:bg-sky-200 disabled:bg-slate-800 dark:disabled:bg-slate-200 font-medium text-slate-200 dark:text-slate-800 shadow enabled:hover:brightness-90 enabled:transition-[filter]${
+            isSuccess === false ? " bg-red-800 dark:bg-red-200" : ""
           }`}
         >
           {isSending
