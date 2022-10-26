1c37f961837d513aca73cd1796eb277d72f51a66
mrcaidev <mrcaidev@gmail.com>
2022-10-26 10:16:21 +0800

    feat: add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         sans: ["Inter"],
       },
+      animation: {
+        leftslide: "leftslide ease 0.5s backwards",
+        rightslide: "rightslide ease 0.5s backwards",
+        lift: "lift ease 0.5s backwards",
+        pop: "pop ease 0.5s backwards",
+      },
       keyframes: {
         menu: {
           from: {
@@ -24,6 +30,46 @@ module.exports = {
           to: {
             transform: "translateX(-50%) translateY(-50%) scale(1.1)",
             opacity: 0,
+          },
+        },
+        leftslide: {
+          from: {
+            transform: "translateX(-30px)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+        rightslide: {
+          from: {
+            transform: "translateX(30px)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+        lift: {
+          from: {
+            transform: "translateY(30px)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
+        },
+        pop: {
+          from: {
+            transform: "scale(0.8)",
+            opacity: 0,
+          },
+          to: {
+            transform: "scale(1)",
+            opacity: 1,
           },
         },
       },
