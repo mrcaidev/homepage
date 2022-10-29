f63a80e81a367a8668223c9d19750bd91dd50fe6
mrcaidev <mrcaidev@gmail.com>
2022-10-29 16:02:12 +0800

    feat: blog pagination

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+interface Page<T> {
+  data: T[];
+  start: number;
+  end: number;
+  total: number;
+  currentPage: number;
+  size: number;
+  lastPage: number;
+  url: {
+    current: string;
+    prev: string | undefined;
+    next: string | undefined;
+  };
+}
--- src/utils/post.ts
+++ src/utils/post.ts
@@ -4,45 +4,56 @@ export interface Frontmatter {
   publishedAt: string;
 }
 
-export const mockPosts = [
+export interface Post extends Frontmatter {
+  slug: string;
+  content: string;
+  readingTime: string;
+}
+
+export const mockPosts: Post[] = [
   {
-    slug: "use-axios-interceptor-with-typescript",
     title: "Use Axios Interceptor with TypeScript",
     description:
       "It is a common practice to set an Axios response interceptor to directly retrieve res.data, but TypeScript's type checking knows nothing about it. How can we inform TypeScript of this change?",
     publishedAt: "2022/8/24",
+    slug: "use-axios-interceptor-with-typescript",
+    content: "Lorem ipsum",
     readingTime: "8 mins read",
   },
   {
-    slug: "use-axios-interceptor-with-typescript",
     title: "Use Axios Interceptor with TypeScript",
     description:
       "It is a common practice to set an Axios response interceptor to directly retrieve res.data, but TypeScript's type checking knows nothing about it. How can we inform TypeScript of this change?",
     publishedAt: "2022/8/23",
+    slug: "use-axios-interceptor-with-typescript",
+    content: "Lorem ipsum",
     readingTime: "8 mins read",
   },
   {
-    slug: "use-axios-interceptor-with-typescript",
     title: "Use Axios Interceptor with TypeScript",
     description:
       "It is a common practice to set an Axios response interceptor to directly retrieve res.data, but TypeScript's type checking knows nothing about it. How can we inform TypeScript of this change?",
     publishedAt: "2022/8/25",
+    slug: "use-axios-interceptor-with-typescript",
+    content: "Lorem ipsum",
     readingTime: "8 mins read",
   },
   {
-    slug: "use-axios-interceptor-with-typescript",
     title: "Use Axios Interceptor with TypeScript",
     description:
       "It is a common practice to set an Axios response interceptor to directly retrieve res.data, but TypeScript's type checking knows nothing about it. How can we inform TypeScript of this change?",
     publishedAt: "2022/8/21",
+    slug: "use-axios-interceptor-with-typescript",
+    content: "Lorem ipsum",
     readingTime: "8 mins read",
   },
   {
-    slug: "use-axios-interceptor-with-typescript",
     title: "Use Axios Interceptor with TypeScript",
     description:
       "It is a common practice to set an Axios response interceptor to directly retrieve res.data, but TypeScript's type checking knows nothing about it. How can we inform TypeScript of this change?",
     publishedAt: "2022/8/27",
+    slug: "use-axios-interceptor-with-typescript",
+    content: "Lorem ipsum",
     readingTime: "8 mins read",
   },
 ];
