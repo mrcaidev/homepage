52d68db7ce8bdfcd307b0854f8fc47456f2da4a8
mrcaidev <mrcaidev@gmail.com>
2023-02-10 00:33:12 +0800

    refactor: colocate data and components

---
+++ src/components/navigation/data.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    text: "About",
+    href: "#about",
+  },
+  {
+    text: "Projects",
+    href: "#projects",
+  },
+  {
+    text: "Contact",
+    href: "#contact",
+  },
+  {
+    text: "Friends",
+    href: "#friends",
+  },
+  {
+    text: "Blog",
+    href: "https://blog.mrcai.dev",
+  },
+];
