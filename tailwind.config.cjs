f7587d44715577cda6fcc908207745b160b2b3d7
mrcaidev <mrcaidev@gmail.com>
2023-02-10 00:50:41 +0800

    refactor: dark mode only

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,11 +8,6 @@ module.exports = {
       },
       animation: {
         cursor: "cursor ease 1s infinite",
-        leftslide: "leftslide ease 0.5s backwards",
-        rightslide: "rightslide ease 0.5s backwards",
-        lift: "lift ease 0.5s backwards",
-        pop: "pop ease 0.5s backwards",
-        drop: "drop ease 0.5s backwards",
       },
       keyframes: {
         cursor: {
@@ -40,56 +35,6 @@ module.exports = {
           to: {
             transform: "translateX(-50%) translateY(-50%) scale(1.1)",
             opacity: 0,
-          },
-        },
-        leftslide: {
-          from: {
-            transform: "translateX(-30px)",
-            opacity: 0,
-          },
-          to: {
-            transform: "translateX(0)",
-            opacity: 1,
-          },
-        },
-        rightslide: {
-          from: {
-            transform: "translateX(30px)",
-            opacity: 0,
-          },
-          to: {
-            transform: "translateX(0)",
-            opacity: 1,
-          },
-        },
-        lift: {
-          from: {
-            transform: "translateY(30px)",
-            opacity: 0,
-          },
-          to: {
-            transform: "translateY(0)",
-            opacity: 1,
-          },
-        },
-        drop: {
-          from: {
-            transform: "translateY(-30px)",
-            opacity: 0,
-          },
-          to: {
-            transform: "translateY(0)",
-            opacity: 1,
-          },
-        },
-        pop: {
-          from: {
-            transform: "scale(0.8)",
-            opacity: 0,
-          },
-          to: {
-            transform: "scale(1)",
-            opacity: 1,
           },
         },
       },
