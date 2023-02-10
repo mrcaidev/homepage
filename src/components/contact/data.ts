3fa2795ec9fbcdcfc3c5113371d3777c673d378a
mrcaidev <mrcaidev@gmail.com>
2023-02-11 00:31:50 +0800

    refactor: form prototype

---
+++ src/components/contact/data.ts
@@ -0,0 +1,31 @@
+import Github from "icons/github.astro";
+import Mail from "icons/mail.astro";
+import Tv from "icons/tv.astro";
+import Twitter from "icons/twitter.astro";
+
+export const platforms = [
+  {
+    title: "GitHub",
+    description: "mrcaidev",
+    icon: Github,
+    href: "https://github.com/mrcaidev",
+  },
+  {
+    title: "Twitter",
+    description: "@mrcaidev",
+    icon: Twitter,
+    href: "https://twitter.com/mrcaidev",
+  },
+  {
+    title: "Bilibili",
+    description: "mrcaidev",
+    icon: Tv,
+    href: "https://space.bilibili.com/35364718",
+  },
+  {
+    title: "Mail",
+    description: "mrcaidev@gmail.com",
+    icon: Mail,
+    href: "mailto:mrcaidev@gmail.com",
+  },
+];
--- src/icons/utils.ts
+++ src/icons/utils.ts
@@ -3,3 +3,5 @@ import type { HTMLAttributes } from "astro/types";
 export type IconProps = HTMLAttributes<"svg"> & {
   size?: number;
 };
+
+export type Icon = (props: IconProps) => unknown;
