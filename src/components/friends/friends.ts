0e4f7e2d919d443545b0555a03f9822d66baad04
Yuwang Cai <mrcaidev@gmail.com>
2024-05-25 23:17:44 +0800

    feat: friends section

---
+++ src/components/friends/friends.ts
@@ -0,0 +1,38 @@
+import mrThanlon from "./avatars/mr-thanlon.webp";
+import qyInvoLing from "./avatars/qy-invo-ling.webp";
+import sake from "./avatars/sake.webp";
+import sumijie from "./avatars/sumijie.webp";
+import yaossg from "./avatars/yaossg.webp";
+
+export const friends = [
+  {
+    name: "Sake",
+    description: "Pythonist. Machine Learning. Computer vision.",
+    link: "https://fullstack-sake.github.io",
+    avatar: sake,
+  },
+  {
+    name: "sumijie",
+    description: "Java developer. Minecraft mod creator. ACMer.",
+    link: "https://focess.top",
+    avatar: sumijie,
+  },
+  {
+    name: "QyInvoLing",
+    description: "Silly web developer. Modest self-describer.",
+    link: "https://qyinvoling.tech",
+    avatar: qyInvoLing,
+  },
+  {
+    name: "MrThanlon",
+    description: "Sell code for living. Impl Bug for Code.",
+    link: "https://blog.ch34k.xyz",
+    avatar: mrThanlon,
+  },
+  {
+    name: "Yaossg",
+    description: "C++ programmer. Hardcore Minecraft player.",
+    link: "https://yaossg.com",
+    avatar: yaossg,
+  },
+];
